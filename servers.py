8270bf01d40dd8755605545fe4b23bfa09f63114
James Aylett <james@tartarus.org>
2011-01-16 19:53:05 +0000

    I believe, with admittedly limited proof, that this will prevent problems with forking children lying around and preventing safe respawn.
    (But don't use the forking version anyway. Threads are your friends.)

--- servers.py
+++ servers.py
@@ -19,6 +19,21 @@ class ThreadedServer(RandomWaitMixin, SocketServer.ThreadingMixIn, WSGIServer):
 class ForkedServer(RandomWaitMixin, SocketServer.ForkingMixIn, WSGIServer):
     def __init__(self, server_address, RequestHandlerClass=None):
         BaseHTTPServer.HTTPServer.__init__(self, server_address, RequestHandlerClass)
+        
+    def finish_request(self, request, client_address):
+        # the process created by ForkingMixin.process_request will be used solely to
+        # run a single leg of that method, which basically calls *this* method then
+        # handles an error if necessary, and exits. It *should* drop its reference
+        # to the listening socket at the start of the child process, but it doesn't.
+        # This is the most convenient way of monkeypatching it in.
+        #
+        # Not dropping the socket means that if the listening process wants to reopen
+        # the socket (which happens in Django's test server if code reloading is on)
+        # then *any* still-running child process will cause that operation to fail. Django
+        # can't recover from this at the moment; the child processes hang around (and
+        # have to be killed manually), while the parent dies. Let's avoid that.
+        self.socket.close()
+        return super(SocketServer.ForkingMixIn, self).finish_request(request, client_address)
 
 def run(addr, port, wsgi_handler):
     server_address = (addr, port)
